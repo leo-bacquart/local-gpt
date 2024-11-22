1d0ffb5a120764883d2eb90044710cfd2ee282df
Léo Bacquart <leo.bacquart@gmail.com>
2024-11-22 16:27:03 +0100

    Température + streaming implémenté

--- app_non_rag.py
+++ app_non_rag.py
@@ -1,14 +1,41 @@
 import streamlit as st
-from langchain.llms import Ollama
+from langchain_core.callbacks import BaseCallbackHandler
+from langchain.chat_models import ChatOllama
+from langchain_core.messages import AIMessage, HumanMessage
 
-llm = Ollama(model='llama3.2')
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
 
 st.title('LLama chatbot without RAG')
 
 question = st.text_input("Ask a question:")
-#TODO: Implémenter la température
 temperature = st.slider("Temperature", 0.0, 1.0, 0.7)
 
 if st.button("Submit"):
-    response = llm.invoke(question)
-    st.write(response)
+    llm = ChatOllama(
+        model='llama3.2',
+        temperature=temperature,
+        disable_streaming=False
+    )
+
+    response_placeholder = st.empty()
+
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self):
+            self.text = ""
+
+        def on_llm_new_token(self, token: str, **kwargs):
+            self.text += token
+            response_placeholder.markdown(self.text)
+
+
+    handler = StreamHandler()
+
+    message = [HumanMessage(content=question)]
+
+    llm(message, callbacks=[handler])
